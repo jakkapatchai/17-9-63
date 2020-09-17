c08d2310bdd37bacd02797e76f3e5aba6f47800a
jakkapatchai <45837555+jakkapatchai@users.noreply.github.com>
2020-09-17 14:28:45 +0700

    Add files via upload

---
+++ select.php
@@ -0,0 +1,9 @@
+﻿<?php
+$connect = new mysqli("localhost","root","","mysql");
+$query = "select * from car";
+$result = $connect->query($query);
+while ($object = $result->fetch_object()) {
+echo $object->f1 . "<br/>";
+}
+$connect->close();
+?>
